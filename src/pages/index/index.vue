659a97e6ebfa0db231c6ecf0fcb2c5ecb6e8efc9
chen-qianshan <1564693736@qq.com>
2025-09-15 14:37:20 +0800

    "001"

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -10,10 +10,10 @@
     </view>
 
     <!-- 写日记按钮 -->
-    <!-- <WriteButton /> -->
+    <WriteButton />
 
     <!-- 底部导航栏 -->
-    <!-- <TabBar ref="tabBar" /> -->
+    <TabBar ref="tabBar" />
   </view>
 
 </template>
